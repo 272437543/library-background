668593737e7816e48f08f5199968dd5c130d6f65
272437543 <30482651+272437543@users.noreply.github.com>
2019-02-28 13:47:19 +0800

    first commit

---
+++ src/main/java/com/drake/library/LibraryBackgroundApplication.java
@@ -0,0 +1,13 @@
+package com.drake.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBackgroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryBackgroundApplication.class, args);
+	}
+
+}
